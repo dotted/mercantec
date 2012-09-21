99e4e77412b2b6a726440ec996718f0d2a2f480f
Bo Fjord Jensen <bo@bxd.dk>
2012-09-21 09:05:37 +0200

    Add CS2

---
+++ h2/csharp/CS2/DriveRPSGameConsole.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace CS2
+{
+    class DriveRPSGameConsole
+    {
+        /// <summary>
+        /// RSPGame      Bishop and Horspool    August 2002
+        /// =======
+        /// Plays the Rock-Paper-Scissors game with the user,
+        /// using the Console for input-output
+        /// </summary>
+        void Go()
+        {
+            /// <summary>
+            /// Drives the Rock-Paper-Scissors game.
+            /// </summary>
+            RPSGame game = new RPSGame();
+            int noOfWins, noOfDraws, noOfLosses, round;
+            noOfWins = noOfDraws = noOfLosses = round = 0;
+            string computersChoice;
+            string result;
+            for (; ; )
+            {
+                computersChoice = game.ComputersChoice;
+                string playersChoice = null;
+                do
+                {
+                    Console.Write(
+                      "Enter R (Rock), P (Paper), " +
+                      "S (Scissors), or Q (Quit): ");
+                    string b = Console.ReadLine().ToLower();
+                    switch (b[0])
+                    {
+                        case 'r':
+                            playersChoice = "Rock"; break;
+                        case 'p':
+                            playersChoice = "Paper"; break;
+                        case 's':
+                            playersChoice = "Scissors"; break;
+                        case 'q':
+                            playersChoice = "Quit"; break;
+                    }
+                } while (playersChoice == null);
+                if (playersChoice == "Quit") break;
+                result = game.ComparePlays(playersChoice);
+                round++;
+                Console.WriteLine("Round " + round);
+                Console.WriteLine("The computer's choice = " + computersChoice);
+                Console.WriteLine("The player's choice = " + playersChoice);
+                switch (result)
+                {
+                    case "draw":
+                        Console.WriteLine("  This round is drawn");
+                        noOfDraws++;
+                        break;
+                    case "lose":
+                        Console.WriteLine("  Sorry, you lose this round");
+                        noOfLosses++;
+                        break;
+                    case "win":
+                        Console.WriteLine("  Well done, you win this round");
+                        noOfWins++;
+                        break;
+                }
+                Console.WriteLine("Status: {0} wins, {1} draws," +
+                          "{2} losses", noOfWins,
+                  noOfDraws, noOfLosses);
+            }
+        }
+        static void Main()
+        {
+            new DriveRPSGameConsole().Go();
+        }
+    }
+}
---
+++ h2/csharp/CS2/RPSGame.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CS2
+{
+    class RPSGame
+    {
+        /// <summary>
+        /// Keeps track of state of the Rock-Paper-Scissors game.
+        /// </summary>
+        string[] MoveNames = { "Rock", "Paper", "Scissors" };
+        string computersChoice;
+        Random r;
+        public RPSGame()
+        {
+            r = new Random();
+        }
+        public string ComputersChoice
+        {
+            get
+            {
+                // generate a random 0, 1 or 2 and convert it to a string
+                // and save it for the compare phase
+                computersChoice = MoveNames[r.Next(3)];
+                return computersChoice;
+            }
+        }
+
+        public string ComparePlays(string playersChoice)
+        {
+            /// <summary>
+            /// Determines whether the player beats the computer.
+            /// Calls constructMessage where the parameters are
+            /// - the player's choice
+            /// - the choice the player could beat
+            /// - the choice that could beat the player.
+            /// </summary>
+            switch (playersChoice)
+            {
+                case "Rock":
+                    //             Player P win     P lose
+                    return Result("Rock", "Scissors", "Paper");
+                case "Paper":
+                    //             Player   P win   P lose
+                    return Result("Paper", "Rock", "Scissors");
+                case "Scissors":
+                    //             Player      P win    P lose
+                    return Result("Scissors", "Paper", "Rock");
+                default:
+                    return null;
+            }
+        }
+
+        string Result(string player, string Pwin, string Plose)
+        {
+            if (computersChoice == Pwin)
+                return "win";
+            else if (computersChoice == Plose)
+                return "lose";
+            else
+                return "draw";
+        }
+    }
+}

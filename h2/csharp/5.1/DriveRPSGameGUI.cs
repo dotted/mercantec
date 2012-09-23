0757acb500954dc59c61f78880f8e520e7e34e6f
Bo Fjord Jensen <bo@bxd.dk>
2012-09-23 22:33:26 +0200

    Add 5.1 Add refernce to Views.dll

---
+++ h2/csharp/5.1/DriveRPSGameGUI.cs
@@ -0,0 +1,122 @@
+﻿using Views;
+using System;
+
+namespace _5._1
+{
+    class DriveRPSGameGUI
+    {
+        /// <summary>
+        /// RSPGame with GUI      Bishop and Horspool      May 2002 
+        /// ================
+        /// Plays the Rock-Paper-Scissors game with the user.
+        /// Illustrates GUI interaction, and switch on string.
+        /// Uses the class RSPSGame from Chapter 4.
+        /// </summary>
+        string fspec =
+          @"<form Text='Rock-Paper-Scissors'>
+      <vertical>
+        <Label Text='Rock-Paper-Scissors' ForeColor=Red Font=Bold18/>
+        <horizontal>
+          <vertical>
+            <space Height=1cm/>
+            <Label Text=Wins/>
+            <TextBox Name=winBox Width=2cm/>
+            <space Height=0.5cm/>
+            <Label Text=Losses/>
+            <TextBox Name=lossBox Width=2cm/>
+            <space Height=0.5cm/>
+            <Label Text=Draws/>
+            <TextBox Name=drawBox Width=2cm/>
+          </vertical>
+          <vertical>
+            <Label Text=History/>
+            <ListBox Name=history Width=7cm/>
+          </vertical>
+        </horizontal>
+        <space Width=10cm Height=0.1cm BackColor=Black
+                 halign=center/>
+        <Label Text='Choose your selection ...' ForeColor=Red/>
+        <horizontal>
+          <Button Name=Rock Image='Rock.gif' Width=3cm/>
+          <Button Name=Paper Image='Paper.gif' Width=3cm/>
+          <Button Name=Scissors Image='Scissors.gif' Width=3cm/>
+        </horizontal>
+      </vertical>
+    </Form>";
+
+        string playersChoice;
+
+        void Go()
+        {
+            /// <summary>
+            /// Drives the Rock-Paper-Scissors game.
+            /// </summary>
+
+            RPSGame game = new RPSGame();
+            int noOfWins, noOfDraws, noOfLosses, round;
+
+            Views.Form form = new Views.Form(fspec);
+            noOfWins = noOfDraws = noOfLosses = 0;
+            round = 0;
+            form.PutText("drawBox", "0");
+            form.PutText("winBox", "0");
+            form.PutText("lossBox", "0");
+
+            string computersChoice, c, result;
+
+            for (; ; )
+            {
+                computersChoice = game.ComputersChoice;
+                c = form.GetControl();
+                if (c == null) break;
+                ActionPerformed(c);  // sets playersChoice
+                result = game.ComparePlays(playersChoice);
+
+                round++;
+                form.PutText("history", "Round " + round);
+                form.PutText("history",
+                       "The computer's choice = " + computersChoice);
+                form.PutText("history",
+                       "The player's choice = " + playersChoice);
+
+                switch (result)
+                {
+                    case "draw":
+                        form.PutText("history", "  This round is drawn");
+                        noOfDraws++;
+                        form.PutText("drawBox", noOfDraws.ToString());
+                        break;
+                    case "lose":
+                        form.PutText("history", "  Sorry, you lose this round");
+                        noOfLosses++;
+                        form.PutText("lossBox", noOfLosses.ToString());
+                        break;
+                    case "win":
+                        form.PutText("history", "  Well done, you win this round");
+                        noOfWins++;
+                        form.PutText("winBox", noOfWins.ToString());
+                        break;
+                }
+                form.PutText("history", "");
+            }
+            form.CloseGUI();
+        }
+
+        void ActionPerformed(string c)
+        {
+            switch (c)
+            {
+                case "Rock":
+                case "Paper":
+                case "Scissors": playersChoice = c;
+                    break;
+                default:
+                    throw new Exception("Unhandled control " + c);
+            }
+        }
+        static void Main()
+        {
+            new DriveRPSGameGUI().Go();
+        }
+    }
+}
---
+++ h2/csharp/5.1/RPSGame.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _5._1
+{
+    class RPSGame
+    {
+        /// <summary>
+        /// Keeps track of state of the Rock-Paper-Scissors game.
+        /// </summary>
+        string[] MoveNames = { "Rock", "Paper", "Scissors" };
+        string computersChoice;
+        Random r;
+        /// <summary>
+        /// Constructor
+        /// Upon instantiation of the class, create instance of Random as r.
+        /// </summary>
+        public RPSGame()
+        {
+            r = new Random();
+        }
+        /// <summary>
+        /// Property
+        /// </summary>
+        public string ComputersChoice
+        {
+            get
+            {
+                // generate a random 0, 1 or 2 and convert it to a string
+                // and save it for the compare phase
+                computersChoice = MoveNames[r.Next(3)];
+                return computersChoice;
+            }
+        }
+        /// <summary>
+        /// Method
+        /// </summary>
+        /// <param name="playersChoice">The choice made by the player</param>
+        /// <returns></returns>
+        public string ComparePlays(string playersChoice)
+        {
+            /// <summary>
+            /// Determines whether the player beats the computer.
+            /// Calls constructMessage where the parameters are
+            /// - the player's choice
+            /// - the choice the player could beat
+            /// - the choice that could beat the player.
+            /// </summary>
+            switch (playersChoice)
+            {
+                //if playersChoice is Rock then return Result with defined params
+                case "Rock":
+                    //             Player P win     P lose
+                    return Result("Rock", "Scissors", "Paper");
+                case "Paper":
+                    //             Player   P win   P lose
+                    return Result("Paper", "Rock", "Scissors");
+                case "Scissors":
+                    //             Player      P win    P lose
+                    return Result("Scissors", "Paper", "Rock");
+                // If playerChoice is none of the 3 above return null
+                default:
+                    return null;
+            }
+        }
+        /// <summary>
+        /// Method
+        /// Compare the parameters with computersChoice property
+        /// Returns string: win, lose or draw.
+        /// </summary>
+        /// <param name="player">Choice of the player</param>
+        /// <param name="Pwin">Losing computer choice</param>
+        /// <param name="Plose">Winning computer choice</param>
+        /// <returns></returns>
+        string Result(string player, string Pwin, string Plose)
+        {
+            if (computersChoice == Pwin)
+                return "win";
+            else if (computersChoice == Plose)
+                return "lose";
+            else
+                return "draw";
+        }
+    }
+}

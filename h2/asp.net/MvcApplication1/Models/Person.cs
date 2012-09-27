4311b6b0d62c7553a48d23b1c2d5ee59a4282d70
Bo Fjord Jensen <bo@bxd.dk>
2012-09-27 10:41:00 +0200

    New project

---
+++ h2/asp.net/MvcApplication1/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace MvcApplication1.Models
+{
+    public class PersonDb : DbContext
+    {
+        public DbSet<Person> MvcApplication1 { get; set; }
+    }
+}
---
+++ h2/asp.net/MvcApplication1/Models/PersonDb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Person
+    {
+        public virtual int id { get; set; }
+        public virtual string name { get; set; }
+    }
+}
